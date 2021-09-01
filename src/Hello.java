66c6088a4f0054bf873026f98b14cb2bc151dc87
Mac_1 <Mac_1@DESKTOP-P3QHQ70>
2021-09-01 11:39:42 +0900

    11

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,10 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Java World!!!");
-		System.out.println("Hi~~~");
+		System.out.println("Hi~~~/////");
+
+		System.out.println("Hi~~~/////");
+		
 	}
 
 }
